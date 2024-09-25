6816be525bd4b510850a9c1039f4e78bbbc787c3
Himalaglebruhv27 <slavaseannextgeneration@gmail.com>
2024-09-25 19:50:35 +0800

    Update update.sh

---
+++ update.sh
@@ -0,0 +1,26 @@
+# Update Script
+# Script created by Himalaglebruhv
+
+
+dependencies() {
+
+command -v git > /dev/null 2>&1 || { echo >&2 "Package GIT is not installed ... Unable to update ..."; exit 1; }
+
+}
+
+script() {
+
+clear
+printf "\n \e[1;92mUpdating \e[1;94mShellPhish\e[1;92m directory ...\n\n"
+sleep 1.5
+cd ..
+rm -rf ShellPhish
+git clone https://github.com/Himalagle-exc/ShellPhish
+cd ShellPhish
+chmod +x *
+printf "\n\e[1;92m Update Complete ...\n\e[0m"
+
+}
+
+dependencies
+script
